666b79e244e9b7b18e2bd10fb3203e2817d8ff26
marcelcastrolima <141673424+marcelcastrolima@users.noreply.github.com>
2023-12-18 09:58:24 -0800

    Improved and cleaned JS

--- script.js
+++ script.js
@@ -1,124 +1,69 @@
+const workListTable = document.querySelector("#work-list-table");
+let sortedProducts = []; // Store sorted data here
+
+const inputField = document.querySelector("#filterInput");
+inputField.addEventListener("keyup", filterTable);
+
+const workList = document.querySelector("#work-list");
+const tableHeaders = workList.querySelectorAll("th");
 
 fetch("workslist.json")
-	.then((response) => response.json())
-	.then(function(products){
-		//console.log(products);
-		const workListTable = document.querySelector("#work-list-table");
-		let workListTableHTML = "";
-		for(let product of products){
-			workListTableHTML += `
-				<tr>
-					<td><strong>${product.title}</strong></td>
-					<td>${product.year}</td>
-					<td>${product.instrumentation}</td>
-					<td>${product.duration}</td>
-					<td>${product.premiere_date}</td>
-					<td>${product.premiered_by}</td>
-				</tr>
-			`;
-		}
-		workListTable.innerHTML = workListTableHTML;
-
-		//Sorts the table by year, descending, when first loaded
-		sortTable(1, "desc");
-	});
-
-
-function sortTable(n, dir="asc") {
+  .then((response) => response.json())
+  .then((products) => {
+    sortedProducts = products.slice(); // Create a copy for sorting
+    renderWorkList(sortedProducts);
+    sortTable('year', "desc"); // Initial sort
 	
-	let switchcount = 0;
-	let table = document.getElementById("work-list");
-	let switching = true;
-	let shouldSwitch;
+  });
 
-	/* Make a loop that will continue until
-	no switching has been done: */
-	while (switching) {
-
-		// Start by saying: no switching is done:
-		switching = false;
-		let rows = table.rows;
-		
-		/* Loop through all table rows (except the
-		first, which contains table headers): */
-		for (i = 1; i < (rows.length - 1); i++) {
-			// Start by saying there should be no switching:
-			shouldSwitch = false;
-			/* Get the two elements you want to compare,
-			one from current row and one from the next: */
-			let x = rows[i].getElementsByTagName("TD")[n];
-			let y = rows[i + 1].getElementsByTagName("TD")[n];
-
-			/* Check if the two rows should switch place,
-			based on the direction, asc or desc: */
-			if (dir == "asc") {
-				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-					// If so, mark as a switch and break the loop:
-					shouldSwitch = true;
-					break;
-				}
-			} else if (dir == "desc") {
-				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-					// If so, mark as a switch and break the loop:
-					shouldSwitch = true;
-					break;
-				}
-			}
-		}
-		if (shouldSwitch) {
-			/* If a switch has been marked, make the switch
-			and mark that a switch has been done: */
-			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-			switching = true;
-			// Each time a switch is done, increase this count by 1:
-			switchcount ++;
-			console.log(switchcount);
-		} else {
-			/* If no switching has been done AND the direction is "asc",
-			set the direction to "desc" and run the while loop again. */
-			if (switchcount == 0 && dir == "asc") {
-				dir = "desc";
-				switching = true;
-			}
-		}
-	}
+function renderWorkList(products) {
+  workListTable.innerHTML = ""; // Clear table before rendering
+  for (const product of products) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td><strong>${product.title}</strong></td>
+      <td>${product.year}</td>
+      <td>${product.instrumentation}</td>
+      <td>${product.duration}</td>
+      <td>${product.premiere_date}</td>
+      <td>${product.premiered_by}</td>
+    `;
+    workListTable.appendChild(row);
+  }
 }
+
+tableHeaders.forEach((header, index) => {
+  const columns = ['title', 'year', 'instrumentation'];
+  const columnIndex = columns[index];
+  header.addEventListener("click", () => {
+    sortTable(columnIndex, header.sortDirection || "asc");
+    header.sortDirection = header.sortDirection === "asc" ? "desc" : "asc";
+  });
+});
+
+function sortTable(n, dir = "asc") {
+	sortedProducts.sort((a, b) => {
+	  const x = a[n].toLowerCase();
+	  const y = b[n].toLowerCase();
+	  if (dir === "asc") return x.localeCompare(y);
+	  return y.localeCompare(x);
+	});
+	renderWorkList(sortedProducts); // Re-render with sorted data
+  }
 
 function filterTable() {
-
-	// Declare variables
-	let input, filter, table, tr, td, i, txtValue;
-	input = document.getElementById("filterInput");
-	filter = input.value.toUpperCase();
-	table = document.getElementById("work-list-table");
-	tr = table.getElementsByTagName("tr");
-	let found = false;
-
-	// Loop through all table rows, and hide those who don't match the search query
-	for (i = 0; i < tr.length; i++) {
-
-		// For each row, loop through all cells.
-		tds = tr[i].getElementsByTagName("td")
-		for (j = 0; j < tds.length; j++) {
-			td = tds[j];
-			if (td) {
-				txtValue = td.textContent || td.innerText;
-				if (txtValue.toUpperCase().indexOf(filter) > -1) {
-					found = true;
-					break;
-				} 
-			}
-		}
-
-		// If value was found, the row is displayed, if not, it's hidden.
-		if (found) {
-			tr[i].style.display = "";
-		} else {
-			tr[i].style.display = "none";
-		}
-
-		// Resets found to false.
-		found = false;	  
-	}
+  const input = document.getElementById("filterInput");
+  const filter = input.value.toUpperCase();
+  const rows = workListTable.querySelectorAll("tr");
+  for (const row of rows) {
+    let visible = false;
+    for (const cell of row.querySelectorAll("td")) {
+      const text = cell.textContent || cell.innerText;
+      if (text.toUpperCase().indexOf(filter) > -1) {
+        visible = true;
+        break;
+      }
+    }
+    row.style.display = visible ? "" : "none";
+  }
 }
-		  
